e8f3e1f4b274a93183fdd8a0c798f85604d614b9
Guruh Putra Mahendra <230040043@stikom-bali.ac.id>
2026-05-08 12:15:01 +0800

    Generate Certificate

--- app/Models/Classes.php
+++ app/Models/Classes.php
@@ -3,15 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\ClassSession;
 
 class Classes extends Model
 {
     protected $fillable = [
-    'class_id',
-    'start_time',
-    'end_time',
-    'teacher_id'
-];
+        'class_id',
+        'start_time',
+        'end_time',
+        'teacher_id',
+        'price',
+    ];
 public function sessions()
 
 {
--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -45,5 +46,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function student()
+    {
+        return $this->hasOne(Student::class);
+    }
+
+    public function teacher()
+    {
+        return $this->hasOne(Teacher::class);
     }
 }
